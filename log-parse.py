7325dad4deb5e80eb36d277051e2644b3e16c140
Nick Simms <nsimms@asu.edu>
2025-04-14 00:16:55 -0700

    add regex

--- log-parse.py
+++ log-parse.py
@@ -1,3 +1,15 @@
 import pandas as pd
+import re
 
-print("I am alive")
+#Open Apache.log file as read.
+with open('log-files/Apache.log', 'r') as file:
+    lines = file.readlines()
+
+df = pd.DataFrame(lines)
+
+line  = df.loc[0,0]
+#line_datetime = re.match("+\w +\w +\d +\d:\d\d:\d\d +\d", line)
+line_datetime = re.match("\[\w\w\w \w\w\w \d\d \d\d\:\d\d\:\d\d \d\d\d\d\]", line)[0]
+
+print(line_datetime)
+#print(line)
