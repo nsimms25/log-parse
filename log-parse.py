ed7edd14f492ad8e5a0e50daf1e44b3c42131f60
Nick Simms <nsimms@asu.edu>
2025-04-15 00:39:51 -0700

    fixed df groupby for client_df

--- log-parse.py
+++ log-parse.py
@@ -6,7 +6,7 @@ with open('log-files/Apache.log', 'r') as file:
     lines = file.readlines()
 
 #Regex pattern to get only entries with client information. Thanks regex101.com!
-regex_pattern = r'\[(?P<datetime>[^\]]+)\] \[(?P<type>\S+)\] \[client (?P<client>\S+)\] (?P<reason>\D+): \D+\n'
+regex_pattern = r'\[(?P<datetime>[^\]]+)\] \[(?P<type>\S+)\] \[client (?P<client>\S+)\] (?P<reason>\D+): (?P<what>\S+)\n'
 
 #Apply re pattern to lines if the regex exists, group as a dict and store in list for df creation later.
 parsed_lines = [re.match(regex_pattern, line).groupdict() for line in lines if re.match(regex_pattern, line)]
@@ -14,5 +14,10 @@ parsed_lines = [re.match(regex_pattern, line).groupdict() for line in lines if r
 #List above to DataFrame.
 df = pd.DataFrame(parsed_lines)
 
-print(df.head())
-print(df[df.type == 'error'])
+#print(df.head())
+#print(df[df.type == 'error'])
+
+client_df = df.drop(columns=["datetime"])
+client_df = client_df.groupby(["client", "what", "reason"]).count()
+
+print(client_df.head())
