4c78f0def47c839b0d6712f9065b115ba79e5ff7
Nick Simms <nsimms@asu.edu>
2025-04-14 23:41:59 -0700

    fixed regex

--- log-parse.py
+++ log-parse.py
@@ -6,10 +6,17 @@ with open('log-files/Apache.log', 'r') as file:
     lines = file.readlines()
 
 df = pd.DataFrame(lines)
-
 line  = df.loc[0,0]
-#line_datetime = re.match("+\w +\w +\d +\d:\d\d:\d\d +\d", line)
-line_datetime = re.match("\[\w\w\w \w\w\w \d\d \d\d\:\d\d\:\d\d \d\d\d\d\]", line)[0]
 
-print(line_datetime)
+regex_pattern = r'\[(?P<datetime>[^\]]+)\]'
+parsed_line = [re.match(regex_pattern, line).groupdict()]
+
+print(parsed_line[0]['datetime'])
+
+
+#for line in lines:
+#    if re.match(regex_pattern, line):
+#        parsed_data = re.match(regex_pattern, line)
+#line_datetime = re.match("+\w +\w +\d +\d:\d\d:\d\d +\d", line)
+
 #print(line)
